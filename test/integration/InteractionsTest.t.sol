67329de8f431dc04661976280445812caf142e1b
AztecWarlord <aztecwarlord@AztecWarlord>
2023-07-07 07:34:19 -0700

    inital commit

---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+// unit
+// intergration
+// forked
+// staging <- run test on a mainnet/testnet
+
+// TODO: For you!
+
+import {Test} from "forge-std/Test.sol";
+
+pragma solidity ^0.8.19;
+
+contract InteractionsTest is Test {}
